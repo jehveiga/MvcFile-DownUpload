0d5e61c212216879360ab05aadcd06353d8f80b0
Jefferson Veiga <gelsvsampa@hotmail.com>
2022-11-27 14:01:27 -0300

    Criar a classe Modelo e Controller de File Upload

---
+++ MvcDownUpload/Controllers/FileUploadController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDownUpload.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        // GET: FileUpload
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcDownUpload/Models/UploadFileResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcDownUpload.Models
+{
+    public class UploadFileResult
+    {
+        public int IdArquivo { get; set; }
+        public string Nome { get; set; }
+        public int Tamanho { get; set; }
+        public string Tipo { get; set; }
+        public string Caminho { get; set; }
+    }
+}
